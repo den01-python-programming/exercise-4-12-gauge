60912f3ab0b40451db14244f77a5685f0820fe2c
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 15:55:03 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,16 @@
 import pytest
-import src.exercise
+from src.gauge import Guage
 
-def test_exercise():
-    #implement tests here
-    assert 0 == 0
+def test_exercise(capsys):
+    g = Gauge()
+
+    while(!g.full()):
+        print("Not full! Value: " + g.value())
+        g.increase()
+
+    print("Full! Value: " + g.value())
+    g.decrease()
+    print("Not full! Value: " + g.value())
+
+    out, err = capsys.readouterr()
+    assert out == "Not full! Value: 0\nNot full! Value: 1\nNot full! Value: 2\nNot full! Value: 3\nNot full! Value: 4\nFull! Value: 5\nNot full! Value: 4\n"
